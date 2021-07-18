1d716f6f2a0caee9c97cb895bc5ea25422a051ac
Wabinab <71509699+Wabinab@users.noreply.github.com>
2021-07-18 15:14:05 +0800

    First working model

--- render.py
+++ render.py
@@ -6,6 +6,9 @@ import cv2
 
 import torch
 
+import os
+import signal
+
 import tkinter as tk
 from PIL import Image, ImageTk
 
@@ -23,67 +26,68 @@ def tkinter_design():
     # Capture video frames
     cap = cv2.VideoCapture(0)
 
-    trigger = True
-
     def callback_start():
         # open_video()
         button.destroy()
+        w.destroy()
+
         imageFrame = tk.Frame(master=window, width=600, height=500)
         imageFrame.grid(row=0, column=0, padx=10, pady=2)
         lmain = tk.Label(imageFrame)
         lmain.grid(row=0, column=0)
+
         show_frame(lmain)
 
-    # id = 0
-    #
+
     def show_frame(lmain):
         _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
+        if trigger.get() == "flip frame horizontally":
+            frame = cv2.flip(frame, 1)
+        results = model(frame)
+        results.display(render=True)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image=img)
         lmain.imgtk = imgtk
         lmain.configure(image=imgtk)
-        id = lmain.after(20, lambda: show_frame(lmain))
+        lmain.after(20, lambda: show_frame(lmain))
+
 
     button = tk.Button(text="Start Video", width=12, height=8,
                        bg="green", fg="black", master=frame,
                        command=callback_start)
     button.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
 
-    # -è--------------------------
+    trigger = tk.StringVar(frame)
+    trigger.set("flip frame horizontally")  # default value
 
-    # def callback_end():
-    #     # end_video()
-    #     trigger = False
-
-    # videoframe = tk.Frame(master=window, relief=tk.FLAT, height=500, width=600)
-    # videoframe.grid(row=1, column=0)
+    w = tk.OptionMenu(frame, trigger, "flip frame horizontally", "don't flip frame")
+    w.grid(row=0, column=1, padx=5, pady=5)
 
     window.mainloop()
 
 
-def open_video():
-    cv2.namedWindow("preview")
-    vc = cv2.VideoCapture(0)
-
-    if vc.isOpened():
-        rval, frame = vc.read()
-    else:
-        rval = False
-
-    while rval:
-        results = model(frame)
-        results.display(render=True)
-        cv2.imshow("preview", frame)
-        rval, frame = vc.read()
-
-        key = cv2.waitKey(20)
-        if key == 27:  # ESC key.
-            break
-
-    vc.release()
-    cv2.destroyWindow("preview")
+# def open_video():
+#     cv2.namedWindow("preview")
+#     vc = cv2.VideoCapture(0)
+#
+#     if vc.isOpened():
+#         rval, frame = vc.read()
+#     else:
+#         rval = False
+#
+#     while rval:
+#         results = model(frame)
+#         results.display(render=True)
+#         cv2.imshow("preview", frame)
+#         rval, frame = vc.read()
+#
+#         key = cv2.waitKey(20)
+#         if key == 27:  # ESC key.
+#             break
+#
+#     vc.release()
+#     cv2.destroyWindow("preview")
 
 
 if __name__ == '__main__':
@@ -91,3 +95,4 @@ if __name__ == '__main__':
 
     tkinter_design()
 
+    os.kill(os.getpid(), signal.SIGTERM)
