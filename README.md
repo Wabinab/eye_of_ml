9dd6121a91face03e64a14d5cc8259004fa3f87f
Wabinab <71509699+Wabinab@users.noreply.github.com>
2021-07-22 17:28:10 +0800

    Take out main function

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ Note this requires a stronger graphics card to run (tested on GTX1080Ti).
 It is recommended to **not** run `pip install -r requirements.txt` directly but to make them yourselves. Particularly, one is not sure how to get distributable from pytorch into requirements.txt hence requires you to install pytorch with cuda yourself, using the code defined from [Pytorch repository](https://pytorch.org/get-started/locally/). Particularly, choose the one with CUDA support according to your OS and CUDA version.  
 
 Run the program with: `python -m render` or `python3 -m render` or perhaps `python -m render.py` or `python3 -m render.py` depending on which one works for your os and which one doesn't raise an error. 
+
+### TensorFlow Models
+If you would like to use TensorFlow models, there are additional installations required to run. 
+
+If you are using Linux (or perhaps Mac OS?), you could run install.sh (after giving it permission using chmod). If it doesn't
+execute properly, change `python` to `python3`. 
+
+If you are on Windows, please follow the instruction [here](https://grpc.io/docs/protoc-installation/).
+To summarize the instruction, go to [here](https://github.com/protocolbuffers/protobuf/releases) and download the 
+`protoc-<version>-win64.zip`, unzip, set environment variable. More information refer to the instruction. Then, 
+run the `install.bat` file. 
+
+If you would like to use gpu, perhaps check with:
+```python
+import tensorflow as tf
+tf.test.is_built_with_cuda()  # should output True
+tf.config.list_physical_devices("GPU")  # Should found your GPU. 
+```
+If not, perhaps install `tensorflow-gpu>=2.5` after doing the above steps as the above step will 
+install tensorflow. 
+
+**Note for tensorflow**: Some large models that aren't download will require some time to download if this is the first
+time open. The screen will freeze (i.e. **Not responding**) while downloading. Monitor the command prompt/terminal for more information on progress. 
+Also for tensorflow models, it takes more time to load than for PyTorch's Yolov5 so requires wait for the model to load.
+**Generally, it takes quite long to load with TF models. Exact timing is not available**. 
--- render.py
+++ render.py
@@ -1,16 +1,21 @@
 """
 Object detection algorithm.
 """
+import re
+
 import numpy as np
 import cv2
 
 import torch
+from torch.backends import cudnn
 
 import os
 import signal
 
 import tkinter as tk
 from PIL import Image, ImageTk
+
+from myutils import *
 
 
 def tkinter_design():
@@ -30,16 +35,19 @@ def tkinter_design():
         # open_video()
         button.destroy()
         w.destroy()
+        mod_menu.destroy()
+
+        model = model_selection(chosen_model, ALL_MODELS)
 
         imageFrame = tk.Frame(master=window, width=1280, height=720)
         imageFrame.grid(row=0, column=0, padx=10, pady=2)
         lmain = tk.Label(imageFrame)
         lmain.grid(row=0, column=0)
 
-        show_frame(lmain, trigger)
-
-
-
+        if chosen_model.get() == "Yolov5":
+            show_frame_pytorch(lmain, trigger, model)
+        else:
+            show_frame_tensorflow(lmain, trigger, model, cat_index, COCO17_HUMAN_POSE_KEYPOINTS)
 
 
     button = tk.Button(text="Start Video", width=12, height=8,
@@ -50,61 +58,105 @@ def tkinter_design():
     trigger = tk.StringVar(frame)
     trigger.set("flip frame horizontally")  # default value
 
-    w = tk.OptionMenu(frame, trigger, "flip frame horizontally", "don't flip frame")
-    w.grid(row=0, column=1, padx=5, pady=5)
+    button_frame = tk.Frame(master=frame, relief=tk.FLAT)
+    button_frame.grid(row=0, column=1, padx=5, pady=5)
+
+    w = tk.OptionMenu(button_frame, trigger, "flip frame horizontally", "don't flip frame")
+    w.grid(row=0, column=0, padx=5, pady=5)
+
+
+    ALL_MODELS, COCO17_HUMAN_POSE_KEYPOINTS, _, cat_index = initialize()
+    models_list = set(ALL_MODELS.keys())
+
+    # Choose only EfficientDet models
+    r = re.compile(r'EfficientDet([\D]+\d)')
+    models_list = sorted([*filter(r.match, models_list)])
+    # models_list = sorted([r.match(f).group() for f in models_list if r.search(f) != None])
+    models_list.insert(0, "Yolov5")
+
+    chosen_model = tk.StringVar(frame)
+    chosen_model.set("Yolov5")
+
+    mod_menu = tk.OptionMenu(button_frame, chosen_model, *models_list)
+    mod_menu.grid(row=1, column=0, padx=5, pady=5)
 
     window.mainloop()
 
 
-def show_frame(lmain, trigger):
+def show_frame_pytorch(lmain, trigger, model):
     _, frame = cap.read()
     if trigger.get() == "flip frame horizontally":
         frame = cv2.flip(frame, 1)
+
     results = model(frame)
     results.display(render=True)
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     img = Image.fromarray(cv2image)
     img = img.resize((1280, 960), Image.ANTIALIAS)
+
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
-    lmain.after(20, lambda: show_frame(lmain, trigger))
+    lmain.after(20, lambda: show_frame_pytorch(lmain, trigger, model))
 
 
-# def open_video():
-#     cv2.namedWindow("preview")
-#     vc = cv2.VideoCapture(0)
-#
-#     if vc.isOpened():
-#         rval, frame = vc.read()
-#     else:
-#         rval = False
-#
-#     while rval:
-#         results = model(frame)
-#         results.display(render=True)
-#         cv2.imshow("preview", frame)
-#         rval, frame = vc.read()
-#
-#         key = cv2.waitKey(20)
-#         if key == 27:  # ESC key.
-#             break
-#
-#     vc.release()
-#     cv2.destroyWindow("preview")
+def show_frame_tensorflow(lmain, trigger, model, category_index, COCO17_HUMAN_POSE_KEYPOINTS):
+    _, frame = cap.read()
+    if trigger.get() == "flip frame horizontally":
+        frame = cv2.flip(frame, 1)
+
+    results = model(np.expand_dims(frame, 0))
+    result = {key:value.numpy() for key,value in results.items()}
+    label_id_offset = 0
+    keypoints, keypoint_scores = None, None
+    if 'detection_keypoints' in result:
+        keypoints = result['detection_keypoints'][0]
+        keypoint_scores = result['detection_keypoint_scores'][0]
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        frame[0],
+        result['detection_boxes'][0],
+        (result['detection_classes'][0] + label_id_offset).astype(int),
+        result['detection_scores'][0],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh=.30,
+        agnostic_mode=False,
+        keypoints=keypoints,
+        keypoint_scores=keypoint_scores,
+        keypoint_edges=COCO17_HUMAN_POSE_KEYPOINTS)
+
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    img = img.resize((1280, 960), Image.ANTIALIAS)
+
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(20, lambda: show_frame_tensorflow(lmain, trigger, model, category_index, COCO17_HUMAN_POSE_KEYPOINTS))
+
+
+def model_selection(chosen_model, ALL_MODELS):
+    if chosen_model.get() == "Yolov5":
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # Model originally on GPU? If not cast to cpu it doesn't open display.
+
+        if device == "cuda":
+            model = torch.hub.load("ultralytics/yolov5", "yolov5l6", pretrained=True)
+            model.half()
+        else:
+            model = torch.hub.load("ultralytics/yolov5", "yolov5s6", pretrained=True)
+            model.to(device)
+
+    else:
+        model = hub.load(ALL_MODELS[chosen_model.get()])
+
+    return model
 
 
 if __name__ == '__main__':
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # Model originally on GPU? If not cast to cpu it doesn't open display. 
-
-    if device == "cuda":
-        model = torch.hub.load("ultralytics/yolov5", "yolov5l6", pretrained=True)
-        model.half()
-    else:
-        model = torch.hub.load("ultralytics/yolov5", "yolov5s6", pretrained=True)
-        model.to(device)
+    cudnn.benchmark = True
 
     # Capture video frames
     cap = cv2.VideoCapture(0)
